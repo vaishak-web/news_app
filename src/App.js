9fb3cde668ad7f1e5f6bff7d7e5b583f451f5b63
Vaishak Nair <vaishak.web@gmail.com>
2021-08-08 00:30:53 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = "e6b400929c6e48636810b6b5637b91062e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () =>{
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command}) =>{
+                if(command === 'testCommand'){
+                    alert('This code is executed');
+                }
+            }
+        })
+    }, []);
+
+
     return(
         <div>
             <h1>AI</h1>
